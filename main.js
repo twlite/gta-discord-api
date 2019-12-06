a05488d6d9e598cc538d992139c8e6200dd0d68a
Glitch (gta-discord-api) <none>
2019-12-06 17:11:01 +0000

    🚂🐄 Updated with Glitch

--- main.js
+++ main.js
@@ -5,13 +5,17 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 app.get("/", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   let gtaGuild = client.guilds.get(process.env.GTAID);
-  if(!gtaGuild) return res.send({ error: "Cannot find GTA server." });
+  if (!gtaGuild) return res.send({ error: "Cannot find GTA server." });
   let memberCount = gtaGuild.memberCount;
-  res.send({ success: memberCount });
+  res.send({ members: memberCount });
 });
 
-// listen for requests :)
 app.listen(process.env.PORT);
 console.log("Your app is listening on port " + process.env.PORT);
 
